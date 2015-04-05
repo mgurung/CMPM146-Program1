43f23a1ffb5cd48cbc0b9f27b25f6b0c8edfaefa
mgurung <mgurung@ucsc.edu>
2015-04-04 20:47:21 -0700

    Added code to both functions
    First function uses Djikstras to return a shortest path to the
    destination. Second function is similar to the get_step function from
    the dfs example with some slight changes.

--- Program1.py
+++ Program1.py
@@ -3,10 +3,60 @@ from math import sqrt
 from heapq import heappush, heappop
 
 def dijkstras_shortest_path(src, dst, graph, adj):
-  raise NotImplementedError	
+  	
+	dist = {}
+	prev = {}
+
+	distance = 0
+	dist[src] = distance
+	prev[src] = None
+	queue = []
+	heappush(queue, src)
+
+	while queue:
+		node = heappop(queue)
+		
+		if node == dst:
+			break
+
+		neighbors = adj(graph, node)
+		for next_node in neighbors:
+			x1, y1 = node
+			x2, y2 = next_node
+			dx = x2 - x1
+			dy = y2 - y1
+			distance = dist[node] + sqrt(dx*dx+dy*dy)
+			if next_node not in dist or distance < dist[next_node]:
+				dist[next_node] = distance
+				prev[next_node] = node
+				heappush(queue, next_node)
+
+	if node == dst:
+		path = []
+		while node:
+			path.append(node)
+			node = prev[node]
+		path.reverse()
+		return path
+	else:
+		return []
+	
 
 def navigation_edges(level, cell):
-  raise NotImplementedError
+	
+	adj = []
+	x, y = cell
+	for dx in [-1,0,1]:
+		for dy in [-1,0,1]:
+			next_cell = (x + dx, y + dy)
+			dist = sqrt(dx*dx+dy*dy)
+			if dist > 0 and next_cell in level['spaces']:
+				adj.append(next_cell)
+			elif dist > 0 and next_cell in level['waypoints']:
+				adj.append(next_cell)
+
+	return adj
+	
 
 def test_route(filename, src_waypoint, dst_waypoint):
 	level = load_level(filename)
